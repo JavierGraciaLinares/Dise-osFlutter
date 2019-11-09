a035cf210a25b4a1f23831d134cab5fc241cf262
JavierGraciaLinares <javiergraciaoficial@gmail.com>
2019-11-09 13:35:03 +0100

    Scroll Medio

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ScrollPage extends StatelessWidget {
+  final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        scrollDirection: Axis.vertical,
         children: <Widget>[
           _pagina1(),
           _pagina2(),
@@ -14,13 +18,61 @@ class ScrollPage extends StatelessWidget {
   }
 
   _pagina1() {
-    return (Center(
-      child: Text("pagina 1"),
-    ));
+    return Stack(
+      children: <Widget>[_colorFondoPag1(), _imagenFondoPag1(), _textosPag1()],
+    );
   }
 
   _pagina2() {
-    return (Center(
-      child: Text("pagina 2"),
-    ));}
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.white,
+          textColor: Colors.black,
+          child: Padding(child: Text("Bienvenido", style: TextStyle(fontSize: 20.0)),padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 20.0),),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  Widget _colorFondoPag1() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondoPag1() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image(
+          image: AssetImage('assets/scroll-1.png'),
+          fit: BoxFit.cover,
+        ));
+  }
+
+  Widget _textosPag1() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.0,
+          ),
+          Text('11º', style: estiloTexto),
+          Text('Sábado', style: estiloTexto),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 85.0)
+        ],
+      ),
+    );
+  }
 }
